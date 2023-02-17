50a9a8495b34080e67951e570c4779150df0b577
Jose Paolo Sese <jpaolo.sese@gmail.com>
2023-02-17 09:25:54 -0800

    Add profile endpoint

--- routes/users-router.js
+++ routes/users-router.js
@@ -1,10 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const users_service = require('../SERVICE/users-service');
+const { verifyTokenAndPayload } = require('../util/jwt-util');
+
+// Redundant to use the same lines of code
+const getUsername = function(req) {
+    const header = req.headers.authorization;
+    const token = header.split(' ')[1];
+    const tokenPayload = verifyTokenAndPayload(token);
+
+    return tokenPayload.username;
+}
+
+// Create GET /profile endpoint using JSON web token to get username
+router.get('/', async (req, res) => {
+    try {
+        const username = await getUsername(req);
+        const data = users_service.showUser(username);
+        res.status(200).json(data);
+    } catch (err) {
+        res.statusCode = 500;
+        res.json(err);
+    }
+});
 
 
-// Create GET /profile/:username endpoint using JSON web token to get username
 
-// Create PATCH /profile/:username/ name
-// Create PATCH /profile/:username/ address
+// Create PATCH /profile name
+router.patch('/:name', async (req, res) => {
+    try {
+        const data = await getUsername(req)
+        const newName = req.body.name;
+    } catch (err) {
+        if (err.name === "NoInputError") {
+            res.statusCode = 400
+        } else {
+            res.statusCode = 500;
+        }
+        res.json(err);
+    }
+})
+
+
+// Create PATCH /profile address
 // ^ Check if req.body.name or req.body.address
 // If neither, throw error
+
+module.exports = router;
--- service/users-service.js
+++ service/users-service.js
@@ -2,20 +2,34 @@
 // Create editName
 
 const { getUserByUsername, updateAddress, updateName } = require('../DAO/users-dao');
+const NoInputError = require('../ERRORS/no-input-error');
 
 async function showUser(username) {
     const data = await getUserByUsername(username);
-    return data.Item; 
+    const user = data.Item;
+    return user; 
 }
 
 
 async function editAddress(username, address) {
+
+    // Ensure user enters a valid address
+    if (address == '' || address == ' ') {
+        throw new NoInputError('Address must not be left empty');
+    }
+
     const data = await updateAddress(username, address);
     return data.Item;
 }
 
 
 async function editName(username, name) {
+
+    // Ensure user enters a valid name
+    if (name == '' || name == ' ') {
+        throw new NoInputError('Name must not be left empty');
+    }
+
     const data = await updateName(username, name);
     return data.Item;
 }

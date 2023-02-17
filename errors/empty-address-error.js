d30e6429294bc824c574dc51eb8e774afbc992dc
Jose Paolo Sese <jpaolo.sese@gmail.com>
2023-02-17 07:36:36 -0800

    Delete redundant empty-address-error.js

--- errors/empty-address-error.js
+++
@@ -1,6 +0,0 @@
-module.exports = class EmptyAddressError extends Error {
-    constructor(message) {
-        super(message)
-        this.name = "EmptyAddressError";
-    }
-}
--- service/users-service.js
+++ service/users-service.js
@@ -3,7 +3,26 @@
 
 const { getUserByUsername, updateAddress, updateName } = require('../DAO/users-dao');
 
-async function editAddress(username, address) {}
+async function showUser(username) {
+    const data = await getUserByUsername(username);
+    return data.Item; 
+}
 
 
-async function editName(username, name) {}
+async function editAddress(username, address) {
+    const data = await updateAddress(username, address);
+    return data.Item;
+}
+
+
+async function editName(username, name) {
+    const data = await updateName(username, name);
+    return data.Item;
+}
+
+
+module.exports = {
+    showUser,
+    editAddress,
+    editName
+}

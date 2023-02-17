5202f8a14affc7f35f7a123a0ee90096ea5ccca4
Jose Paolo Sese <jpaolo.sese@gmail.com>
2023-02-16 21:44:12 -0800

    Add updateName and updateAddress in users-dao.js

--- dao/users-dao.js
+++ dao/users-dao.js
@@ -20,6 +20,45 @@ const getUserByUsername = (username) => {
   return docClient.get(params).promise();
 };
 
+// Update user's name
+const updateAddress = (username, address) => {
+  const params = {
+    TableName: table,
+    UpdateExpression: "set #add = :add",
+    ExpressionAttributeNames: {
+      "#add": "address"
+    },
+    ExpressionAttributeValues: {
+      ":add": address
+    },
+    Key: {
+      username
+    }
+  }
+
+  return docClient.update(params).promise();
+}
+
+const updateName = (username, newName) => {
+  const params = {
+    TableName: table,
+    UpdateExpression: "set #name = :name",
+    ExpressionAttributeNames: {
+      "#name": "name"
+    },
+    ExpressionAttributeValues: {
+      ":name": newName
+    },
+    Key: {
+      username
+    }
+  }
+
+  return docClient.update(params).promise();
+}
+
 module.exports = {
   getUserByUsername,
+  updateAddress,
+  updateName,
 };

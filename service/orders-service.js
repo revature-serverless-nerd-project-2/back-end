fe971dd90436359aac45a3d8c5961923cd58350b
Kanaan <studio.knntaylor@gmail.com>
2023-02-19 17:25:50 -0600

    accidently replaced a file. fixed

--- service/orders-service.js
+++ service/orders-service.js
@@ -1,62 +1,29 @@
-const {checkout} = require('../service/orders-service');
-const { showCart } = require('../service/cart-service');
+const {createJWT, verifyTokenAndPayload} = require('../util/jwt-util');
+const {addOrders} = require('../dao/orders.js');
 const {retrieveCart} = require('../dao/cart-dao');
-const {addOrders} = require('../dao/orders');
+const {deleteProductByID} = require('../dao/products-dao');
 const NoCartItemsToCheckoutError = require('../errors/no-items-to-checkout-error');
+const uuid = require('uuid');
 
-jest.mock('../dao/cart-dao.js', function() {
-    return {
-        retrieveCart: jest.fn(),
+const timestamp = require('unix-timestamp');
+timestamp.round = true
+
+async function checkout(username) {
+    
+    const data = await retrieveCart(username);
+   const userItems = data.Item;
+   
+   if(!userItems){
        
-    }
-});
+    
+        throw new NoCartItemsToCheckoutError("Items already checked out!");
+      
+}
 
-jest.mock('../dao/orders.js', function() {
-    return {
-        addOrders: jest.fn(),
-       
-    }
-});
+        await addOrders(username, timestamp.now(), userItems);
+    
 
-describe('Checkout tests', () => {
-
-    test('Cart is Empty!' , async () => {
-        retrieveCart.mockReturnValueOnce(Promise.resolve(
-            { userItems : []
-        }));
-
-        await expect(checkout()).rejects.toThrow(NoCartItemsToCheckoutError);
-    });
-
-    // test('Item Successfully checked out', async () => {
-    //     addOrders.mockReturnValueOnce(Promise.resolve({
-    //         Items : {
-    //            username : 'user1', 
-    //            timestamp :'0011', 
-    //            Firstname: 'Jane', 
-    //            Lastname :'Doe', 
-    //            Email :'Email', 
-    //            Address :'Address', 
-    //            Address2 : 'Address2', 
-    //            City : 'City', 
-    //            State : 'State', 
-    //            Zip : 'Zip', 
-    //            userItems :'userItems'} }));
-
-    //     const so = await checkout('user1', 'Jane', 'Doe', 'Email', 'Address', 'Address2', 'City', 'State', 'Zip');
-    //     expect(so).toMatchObject({
-    //     username : 'user1', 
-    //     timestamp :'0011', 
-    //     Firstname: 'Jane', 
-    //     Lastname :'Doe', 
-    //     Email :'Email', 
-    //     Address :'Address', 
-    //     Address2 : 'Address2', 
-    //     City : 'City', 
-    //     State : 'State', 
-    //     Zip : 'Zip', 
-    //     userItems :'userItems'})
-    // })
-
-
-})
+}
+module.exports =  {
+    checkout
+}
